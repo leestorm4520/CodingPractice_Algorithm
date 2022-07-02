adcb25ddec94553a4a5424babe3a0f5ed49b7a2b
DuyLe <minhduy4520@gmail.com>
2022-07-02 11:44:32 -0500

    Update FirstBadVersion.java

--- Graph/Tree/BinaryTree/FirstBadVersion.java
+++ Graph/Tree/BinaryTree/FirstBadVersion.java
@@ -33,7 +33,19 @@ Constraints:
 
 public class FirstBadVersion {
    
-    public int firstBadVersion(int n) {
+    /* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
 
+    public int firstBadVersion(int n) {
+        int start=1, end=n;
+        while(start<end){
+            int mid=start+ (end-start)/2;
+            if(!isBadVersion(mid)) start=mid+1;
+            else end=mid;
+        }
+        return start;
+    }
+   
+}
     
 }
