ef1de96483e58540538af8ee8ac96743951ce4c4
DuyLe <minhduy4520@gmail.com>
2022-07-04 01:29:12 -0500

    Update LongestIncreasingSubsequence.java

--- String Manipulation/LongestIncreasingSubsequence.java
+++ String Manipulation/LongestIncreasingSubsequence.java
@@ -2,11 +2,18 @@ package String Manipulation;
 
 public class LongestIncreasingSubsequence {
     public static void main(String[] args){
-        
-
+        int[] arr={5,2,8,6,3,6,9,5};
+        System.out.println(maxIncreasing(arr));
     }
 
-    public static int findString(String s, String t){
-
+    public static int maxIncreasing(int[] arr){
+        if(arr==null || arr.length==0) return 0;
+        if(arr.length==1) return 1;
+        int maxIncreasing=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i-1]<arr[i]){
+                maxIncreasing=1+Math.max(maxIncreasing, maxIncreasing[0-> (i-1)]);
+            }
+        }
     }
 }
