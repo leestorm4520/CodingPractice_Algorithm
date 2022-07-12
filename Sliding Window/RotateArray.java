a16e53029f90347c67567625d5e064b0de338e4e
DuyLe <minhduy4520@gmail.com>
2022-07-11 20:22:09 -0500

    update

--- Sliding Window/RotateArray.java
+++ Sliding Window/RotateArray.java
@@ -50,6 +50,21 @@ public class RotateArray {
     //    for(int i=0;i<nums.length;i++) nums[i]=result.get(i);
     // }
     public static void rotate (int[] nums, int k){
-        
+        k=k%nums.length;
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+    }
+    public static void swap(int[] nums, int i, int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    public static void reverse(int[] nums, int left, int right){
+        while(left<right){
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
     }
 }
