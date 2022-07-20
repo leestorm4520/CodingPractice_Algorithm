aa8e2d6ad57505d16b040cfe51751bcdc5cd94b6
DuyLe <minhduy4520@gmail.com>
2022-07-20 11:23:11 -0500

    Update MiddleLL.py

--- LinkedList/MiddleLL.py
+++ LinkedList/MiddleLL.py
@@ -22,3 +22,17 @@ Constraints:
 The number of nodes in the list is in the range [1, 100].
 1 <= Node.val <= 100
 '''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    
