d4059df163d05754700bc09d61a411a43b66f223
DuyLe <minhduy4520@gmail.com>
2022-07-19 19:45:49 -0500

    update

---
+++ Sliding Window/ReverseWords.py
@@ -0,0 +1,45 @@
+'''
+Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+ 
+
+Example 1:
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+Example 2:
+
+Input: s = "God Ding"
+Output: "doG gniD"
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+s contains printable ASCII characters.
+s does not contain any leading or trailing spaces.
+There is at least one word in s.
+All the words in s are separated by a single space.
+'''
+def reverseWords(str):
+    newStr=""
+    words=str.split();
+    for word in words:
+        reverse(word)
+        newStr=word+" "
+    return newStr
+
+def reverse(word):
+    l,r=0, len(word)-1
+    while(l<r):
+        tmp=word[l]
+        word[l]=word[r]
+        word[r]=tmp
+
+
+def main():
+    s = "Let's take LeetCode contest"
+    print(reverseWords(s))
+
+if __name__=="__main__":
+    main()
