c36fe6b5e936c11480d9ccb8fe3a326569b662b9
John Le <70299365+leestorm4520@users.noreply.github.com>
2022-10-26 01:57:13 +0000

    update

--- Patterns/Sliding Window/sliding/sliding.py
+++ Patterns/Sliding Window/sliding/sliding.py
@@ -2,13 +2,14 @@ import math
 
 class SlidingWindow:
     #assign value to object properties when initialized
-    def __init__(self, arr, k, max):
+    def __init__(self, arr, k, max, line):
         self.arr=arr
         self.k=k
         self.max = max
+        self.line = line
     def __str__(self):
         return f"{self.arr} - {self.k}"
-    def findAverage(self):
+    def findAverageSubArray(self):
         result = []
         windowSum, windowStart = 0.0, 0
         for windowEnd in range(len(self.arr)):
@@ -20,7 +21,7 @@ class SlidingWindow:
                 windowStart += 1  # slide the window ahead
         return result
     # find smallest subarray with a greater sum
-    def findSmallest(self):
+    def findSmallestSubArray(self):
         windowSum, windowStart, minLength = 0, 0, math.inf
         for windowEnd in range(len(self.arr)):
             windowSum += self.arr[windowEnd] # add the next element
@@ -31,12 +32,45 @@ class SlidingWindow:
         if minLength == math.inf:
             return []
         return self.arr[windowStart:windowEnd]
+    def findLongestSubstring(self):
+      # left is the start of substring
+      # right is the end of substring, being (k-1)th
+      # count is the number of distinct substrings in the current substring
+      left, right, maxLength, count = 0, 0, 0, 0
+      
+       # dictionary to keep track of how often a character shows up in the line
+      dict = {}
+      for character in self.line:
+        dict[character] = 0
+
+      while right < len(self.line):
+        dict[self.line[right]]+=1
+        if dict[self.line[right]] == 0:
+          count+=1
+        while count > self.k:
+          maxLength = max(maxLength, right - left +1)
+          left+=1
+          dict[self.line[left]]-=1
+          if dict[self.line[left]] == 0 :
+            count -=1
+        right+=1
+      return self.line[left:right]
+      
+
+      # while right < len(self.line) - 1 && left < right:
+      #   if arr[right] == 0 && count < k:
+      #     arr[right] = 1
+      #     count++
+      #     right++
+      #   while count > k:
+      #     maxLength = max(maxLength, right - left + 1)
+      #     if 
+      # return maxLength
 
 def main():
-    slidingWindow=SlidingWindow([3,45,-5,7,26,63,1,67], 4, 49)
-    result = slidingWindow.findAverage()
-    print("Averages of subarrays of size K: " + str(result))
-    print("Smallest subarray with a great sum of "+str(slidingWindow.max) + " is " + str(slidingWindow.findSmallest()))
-
+    slidingWindow=SlidingWindow([3,45,-5,7,26,63,1,67], 4, 49, "aaravfffberefessss")
+    print("Averages of subarrays of size K: " + str(slidingWindow.findAverageSubArray()))
+    print("Smallest subarray with a great sum of "+str(slidingWindow.max) + " is " + str(slidingWindow.findSmallestSubArray()))
+    print("Longest substring with "+ str(slidingWindow.k)+" distinct character is "+str(slidingWindow.findLongestSubstring()))
 
 main()
---
+++ replit.nix
@@ -0,0 +1,9 @@
+{ pkgs }: {
+    deps = [
+        pkgs.python39Full
+        pkgs.graalvm17-ce
+        pkgs.maven
+        pkgs.replitPackages.jdt-language-server
+        pkgs.replitPackages.java-debug
+    ];
+}
